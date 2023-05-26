c736e82e4b6daf1278780e57a05ad4385e0fdc50
GABRIEL GONCALVES ALBANEZ <gabriel.galbanez@senacsp.edu.br>
2023-05-26 17:11:53 -0300

    subindo main inicio

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,11 +1,18 @@
-import BackImg from "./Backimg";
+
 
 
 
 export default function Main(){
 return(
     <main>
-        <BackImg/>
+        <section className="bg-neutral-900 h-screen w-screen p-16 flex flex-col gap-20 ">
+           <h1 className="text-white text-4xl">Bem Vindo a  <span className=" text-cyan-500 shadow-sm  shadow-cyan-500/50">ZeusMusic!</span> </h1>
+           <ul className="flex flex-col gap-10">
+            <li className="text-xl text-white">ZuesMusic é um site de musica intuitivo e sofisticado</li>
+            <li className="text-xl text-white">"Navegue por um oceano de gêneros musicais e mergulhe na diversidade"</li>
+            <li className="text-xl text-white"></li>
+           </ul>
+        </section>
     </main>
 )
 
