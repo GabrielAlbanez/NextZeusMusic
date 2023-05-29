eb4833733ad9d1146141ef502a438b6838561197
GABRIEL GONCALVES ALBANEZ <gabriel.galbanez@senacsp.edu.br>
2023-05-29 17:11:08 -0300

    subindo card assinatura

---
+++ src/app/components/Card.jsx
@@ -0,0 +1,17 @@
+
+
+import ZeusCard from '../assets/img/ZeusCard.jpg'
+import PoseidonCard from '../assets/img/Poseidon.jpg'
+import AriesCard from '../assets/img/aires.jpg'
+
+
+
+export default function Card(props) {
+  return (
+    <div className='bg-red-700 w-screen h-96
+    '>
+        <h1 className="text-red-400">Aries</h1>
+       
+    </div>
+  )
+}
--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { motion, useTime, useTransform } from "framer-motion";
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import Image from "next/image";
 import iamge1 from "../assets/img/Acdcx.jpg";
 import iamge2 from "../assets/img/Codplay.jpg";
 import iamge3 from "../assets/img/Enimen.jpg";
 import iamge4 from "../assets/img/Marron FIve.jpg";
+import Card from "./Card";
 
 const imagens = [
   iamge1,
@@ -19,22 +20,14 @@ const imagens = [
   iamge4,
 ];
 
-
-
-
 export default function Main() {
-
-
-  
-
   return (
-    <motion.main 
-     
-    initial={{ opacity: 0 }} 
-  animate={{ opacity: 1 }} 
-  transition={{ duration: 1.5 }}
-    
-    className="bg-gradient-to-tr from-cyan-700  to-neutral-900 to-60% h-screen w-screen">
+    <motion.main
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="bg-neutral-900 h-screen w-screen"
+    >
       <section className=" h-1/2 w-screen p-16 flex flex-col gap-20 justify-center items-center">
         <h1 className="text-white text-7xl">
           Bem Vindo a{" "}
@@ -58,27 +51,23 @@ export default function Main() {
           Artistas mais tocados do momento
         </h1>
         <figure className=" flex items-center justify-center">
-          <motion.div    className="carousel bg-gradient-to-t from-neutral-900  w-screen h-96 flex ">
+          <motion.div className="carousel  w-screen h-96 flex ">
             <motion.div
-          animate={{
-            x: ['0%', '10%', '-10%','0%'],
-            
-          }}
-
-
-          transition={{
-            repeat: Infinity,
-            duration: 6,
-            ease: "linear",
-            yoyo: Infinity,
-          }}
+              animate={{
+                x: ["0%", "10%", "-10%", "0%"],
+              }}
+              transition={{
+                repeat: Infinity,
+                duration: 6,
+                ease: "linear",
+                yoyo: Infinity,
+              }}
               className="flex gap-6 items-center  justify-center "
             >
               {imagens.map((imagenzinha) => (
                 <motion.div
                   key={imagenzinha}
                   className="min-w-400px min-h-200px"
-           
                 >
                   <Image
                     src={imagenzinha}
@@ -90,6 +79,18 @@ export default function Main() {
             </motion.div>
           </motion.div>
         </figure>
+      </section>
+        
+      <section className ="h-3/4 bg-gradient-to-t from-cyan-700  to-neutral-900 to-80% ">
+        <div className="flex items-center justify-center pt-16">
+      <h1 className="text-white text-6xl">Assinaturas</h1>
+      </div>
+        <div className=" seguradora flex justify-evenly items-center gap-10 pt-14 px-10">
+        <Card/>
+        <Card/>
+        <Card/>
+        </div>
+        
       </section>
     </motion.main>
   );
