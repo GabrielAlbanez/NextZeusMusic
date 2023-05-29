ed64f60984676aae4b063d4b7757297d77204b28
GabrielAlbanez <gabriel.g.albanez@gmail.com>
2023-05-29 12:19:56 -0300

    carrousel add

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,16 +1,27 @@
-import Image from 'next/image';
+import Image from "next/image";
 // import Logo from '../assets/img/zuescara2.gif';
 // <Image src={Logo} alt = "logo" className='w-14 h-12'/>
 
 export default function Header() {
   return (
     <header className="bg-zinc-950 flex items-center justify-between h-20 px-10">
-      <div className='flex items-center gap-10'><p className=" text-cyan-500 shadow-sm bg-tra shadow-cyan-500/50 text-3xl">ZeusMusic</p>  </div>
+      <div className="flex items-center gap-10">
+        <p className=" text-cyan-500 shadow-sm bg-tra shadow-cyan-500/50 text-3xl">
+          ZeusMusic
+        </p>{" "}
+      </div>
       <div className="flex gap-3 text-center items-center  w-1/3 justify-evenly pl-24 ">
-        <div className=''><button className="text-white  bg-cyan-500 shadow-lg rounded-lg shadow-cyan-500/50 w-24 h-10 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-blue-500 duration-300">Logar</button></div>
-        <div><button className="text-white  bg-cyan-500 shadow-lg rounded-lg shadow-cyan-500/50 w-24 h-10 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-blue-500 duration-300">Registrar-se</button></div>
+        <div className="">
+          <button className="text-white  bg-cyan-500 shadow-lg rounded-lg shadow-cyan-500/50 w-24 h-10 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-blue-500 duration-300">
+            Logar
+          </button>
+        </div>
+        <div>
+          <button className="text-white  bg-cyan-500 shadow-lg rounded-lg shadow-cyan-500/50 w-24 h-10 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-blue-500 duration-300">
+            Registrar-se
+          </button>
+        </div>
       </div>
     </header>
-  )
+  );
 }
-
--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,20 +1,90 @@
+"use client";
+
+import { motion, useTime, useTransform } from "framer-motion";
+import React, { useEffect } from 'react';
+import Image from "next/image";
+import iamge1 from "../assets/img/Acdcx.jpg";
+import iamge2 from "../assets/img/Codplay.jpg";
+import iamge3 from "../assets/img/Enimen.jpg";
+import iamge4 from "../assets/img/Marron FIve.jpg";
+
+const imagens = [
+  iamge1,
+  iamge2,
+  iamge3,
+  iamge4,
+  iamge1,
+  iamge2,
+  iamge3,
+  iamge4,
+];
 
 
 
 
-export default function Main(){
-return(
-    <main>
-        <section className="bg-neutral-900 h-screen w-screen p-16 flex flex-col gap-20 ">
-           <h1 className="text-white text-4xl">Bem Vindo a  <span className=" text-cyan-500 shadow-sm  shadow-cyan-500/50">ZeusMusic!</span> </h1>
-           <ul className="flex flex-col gap-10">
-            <li className="text-xl text-white">ZuesMusic é um site de musica intuitivo e sofisticado</li>
-            <li className="text-xl text-white">"Navegue por um oceano de gêneros musicais e mergulhe na diversidade"</li>
-            <li className="text-xl text-white"></li>
-           </ul>
-        </section>
+export default function Main() {
+
+
+  
+
+  return (
+    <main className="bg-gradient-to-tr from-cyan-700  to-neutral-900 to-60% h-screen w-screen">
+      <section className=" h-1/2 w-screen p-16 flex flex-col gap-20 justify-center items-center">
+        <h1 className="text-white text-7xl">
+          Bem Vindo a{" "}
+          <span className=" text-cyan-500 shadow-sm  shadow-cyan-500/50">
+            ZeusMusic!
+          </span>{" "}
+        </h1>
+        <div className="flex items-center justify-center gap-20 flex-col pt-10">
+          <p className="text-3xl text-white">
+            "ZuesMusic é um site de musica intuitivo e sofisticado"
+          </p>
+          <p className="text-3xl text-white">
+            "Navegue por um oceano de gêneros musicais e mergulhe na
+            diversidade"
+          </p>
+          <p className="text-2xl text-white"></p>
+        </div>
+      </section>
+      <section className="flex flex-col items-center justify-center gap-10">
+        <h1 className="text-white text-6xl h-1/2 flex justify-center items-center">
+          Artistas mais tocados do momento
+        </h1>
+        <figure className=" flex items-center justify-center">
+          <motion.div    className="carousel bg-gradient-to-t from-neutral-900  w-screen h-96 flex ">
+            <motion.div
+          animate={{
+            x: ['0%', '10%', '-10%','0%'],
+            
+          }}
+
+
+          transition={{
+            repeat: Infinity,
+            duration: 6,
+            ease: "linear",
+            yoyo: Infinity,
+          }}
+              className="flex gap-6 items-center  justify-center "
+            >
+              {imagens.map((imagenzinha) => (
+                <motion.div
+                  key={imagenzinha}
+                  className="min-w-400px min-h-200px"
+           
+                >
+                  <Image
+                    src={imagenzinha}
+                    alt="iamgensCard"
+                    className="w-full h-80 rounded-lg shadow-xl  shadow-cyan-500/50"
+                  />
+                </motion.div>
+              ))}
+            </motion.div>
+          </motion.div>
+        </figure>
+      </section>
     </main>
-)
-
-
+  );
 }
