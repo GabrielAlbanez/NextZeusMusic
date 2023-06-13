4a2439d132a767c72d4fc57c50d05f708787433a
GABRIEL GONCALVES ALBANEZ <gabriel.galbanez@senacsp.edu.br>
2023-06-13 14:49:05 -0300

    responsividade

--- src/app/components/Assinatura.jsx
+++ src/app/components/Assinatura.jsx
@@ -14,13 +14,26 @@ const AssianturaComponent = ({ img, titulo, chindren, descricao }) => {
   const palavraDestaque = <span>1 ano</span>;
 
   return (
-    <div className="bg-gradient-to-tr from-neutral-900 from-40% to-cyan-700 px-10 w-full sm:w-1/2 h-auto sm:h-1/2 flex items-start justify-between border-2 border-white rounded-2xl relative">
-      <div className="flex items-start justify-center h-24 flex-col float-left relative sm:h-10 pt-10">
-        <h1 className="text-white  text-xl w-screen sm:text-4xl absolute mt-12 mb-20 ">Assinatura: {cardName}</h1>
+    <div className="bg-gradient-to-tr from-neutral-900 from-40% to-cyan-700 px-10 w-full sm:w-1/2 h-auto sm:h-1/2 flex items-start justify-evenly border-2 border-white rounded-2xl relative">
+      <div className="flex items-start justify-center h-24 flex-col  sm:h-10 pt-10">
+
+        <div className="flex gap-14 flex-col absolute ">
+          <button
+            onClick={() => {
+              setRender(false);
+            }}
+            className="text-white z-50 absolute"
+          >
+            Voltar
+          </button>
+
+
+        </div>
+
         {cardName === "Zeus" && (
           <PrivilegiosAssinatura
             descricao={"O que a assinatura Zeus lhe proporciona:"}
-            
+
             tempo={`Esta assinatura possibilita você ouvir música sem anúncios por 1 ano`}
             tempo2={"1 ano"}
             privilegio1={"Você tem privilégios em qualidade sonora"}
@@ -33,17 +46,58 @@ const AssianturaComponent = ({ img, titulo, chindren, descricao }) => {
             custo2={"264 reais"}
           />
         )}
+
+        
+{cardName === "Poseidon" && (
+          <PrivilegiosAssinatura
+            descricao={"O que a assinatura Zeus lhe proporciona:"}
+
+            tempo={`Esta assinatura possibilita você ouvir música sem anúncios por 1 ano`}
+            tempo2={"1 ano"}
+            privilegio1={"Você tem privilégios em qualidade sonora"}
+            privilegio1m={"Qualidade sonora +"}
+            privilegio2={"Você pode escutar músicas offline"}
+            privilegio2m={"musicas ofline"}
+            privilegio3={"Você pode compartilhar os privilégios dessa assinatura com até 4 pessoas diferentes"}
+            privilegio3m={"4 acessos simultaneos"}
+            custo={"264 reais"}
+            custo2={"264 reais"}
+          />
+        )}
+
+
+{cardName === "Aries" && (
+          <PrivilegiosAssinatura
+            descricao={"O que a assinatura Zeus lhe proporciona:"}
+
+            tempo={`Esta assinatura possibilita você ouvir música sem anúncios por 1 ano`}
+            tempo2={"1 ano"}
+            privilegio1={"Você tem privilégios em qualidade sonora"}
+            privilegio1m={"Qualidade sonora +"}
+            privilegio2={"Você pode escutar músicas offline"}
+            privilegio2m={"musicas ofline"}
+            privilegio3={"Você pode compartilhar os privilégios dessa assinatura com até 4 pessoas diferentes"}
+            privilegio3m={"4 acessos simultaneos"}
+            custo={"264 reais"}
+            custo2={"264 reais"}
+          />
+        )}
+ 
+
+ 
+
+
       </div>
       <div>
-        <div className="h-96   sm:h-96  pt-9 pb-9 px-10 z-50 flex flex-col justify-between ml-24">
-          <button
+        <div className="h-96   pt-9 pb-9 px-10 z-50 flex flex-col justify-between ml-24">
+          {/* <button
             onClick={() => {
               setRender(false);
             }}
             className="text-white z-50"
           >
             Voltar
-          </button>
+          </button> */}
           <div>
             <button className="text-white z-50 relative bg-cyan-500 shadow-lg rounded-lg shadow-cyan-500/50 w-20 h-9 text-center transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-blue-500 duration-300">
               Assinar
@@ -57,10 +111,10 @@ const AssianturaComponent = ({ img, titulo, chindren, descricao }) => {
             cardName === "Zeus"
               ? ZeusCard
               : cardName === "Poseidon"
-              ? PoseidonCard
-              : cardName === "Aries"
-              ? AriesCard
-              : null
+                ? PoseidonCard
+                : cardName === "Aries"
+                  ? AriesCard
+                  : null
           }
           layout="fill"
           objectFit="cover"
--- src/app/components/PrivilegioAssinatura.jsx
+++ src/app/components/PrivilegioAssinatura.jsx
@@ -13,12 +13,12 @@ const PrivilegiosAssinatura = ({descricao,tempo,tempo2,privilegio1,privilegio1m,
 
   return(
     <ol className="text-white pt-20 relative flex gap-10 flex-col  w-62 h-36 sm:pt-36 gap-5">
-          <li className="w-24 sm:w-full">{tamanhoTela < 400 ? "" : descricao}</li>
-          <li className="w-24 sm:w-full"> {tamanhoTela < 400 ? tempo2 : tempo}</li>
-          <li className="w-24 sm:w-full"> {tamanhoTela < 400 ? privilegio1m : privilegio1}</li>
-          <li className="w-24 sm:w-full">{tamanhoTela < 400 ? privilegio2m : privilegio2}</li>
-          <li className="w-24 sm:w-full">{tamanhoTela < 400 ? privilegio3m : privilegio3}</li>
-          <li className="w-24 sm:w-full">{tamanhoTela < 400 ? custo2 : custo}</li>
+          <li className="w-24 sm:w-full">{tamanhoTela < 1200 ? "" : descricao}</li>
+          <li className="w-24 sm:w-full"> {tamanhoTela < 1200 ? tempo2 : tempo}</li>
+          <li className="w-24 sm:w-full"> {tamanhoTela < 1200 ? privilegio1m : privilegio1}</li>
+          <li className="w-24 sm:w-full">{tamanhoTela < 1200 ? privilegio2m : privilegio2}</li>
+          <li className="w-24 sm:w-full">{tamanhoTela < 1200 ? privilegio3m : privilegio3}</li>
+          <li className="w-24 sm:w-full">{tamanhoTela < 1200 ? custo2 : custo}</li>
         </ol>
   )
 
